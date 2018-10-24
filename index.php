a4be17170104d63d98b985a95f43797366f24982
sumitramanga <sumitramanga@gmail.com>
2018-10-25 11:30:13 +1300

    Added assets and added wp_header and wp_footer

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<?php wp_head(); ?>
+	<title>Infuse - Yoobee School of Design 2018 Exhibiton</title>
+</head>
+<body>
+	<h1>Test</h1>
+
+	<?php wp_footer(); ?>
+</body>
+</html>
