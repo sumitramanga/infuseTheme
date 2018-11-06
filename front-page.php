de855fe73de26d3eb98d30252676cb4cf123bc71
sumitramanga <sumitramanga@gmail.com>
2018-11-07 11:56:05 +1300

    removed spacing

--- front-page.php
+++ front-page.php
@@ -1,13 +1,10 @@
 <?php get_header(); ?>
 
-
 <div class="container main text-center">
 	<h1 class="logoText">Infuse</h1>
 	<p class="lead">
 		Yoobee Wellington Campus Presents The 2018 Graduation Day Of Three Level 6 Courses
 	</p>
 </div>
-
-
 
 <?php get_footer(); ?>
