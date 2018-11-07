4070e429de63c12ee4627e07512fe301e02cb565
sumitramanga <sumitramanga@gmail.com>
2018-11-08 08:59:51 +1300

    fixed merge conflict on fucntions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,8 @@
 <?php
 
 function addCustomThemeStyles() {
-<<<<<<< HEAD
-	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array());
-	wp_enqueue_style('customcss', get_template_directory_uri() . '/assets/css/theme-style.css', array(), '0.0.1', 'all');
-
-=======
 	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '4.1.3', 'all');
 	wp_enqueue_style('customcss', get_template_directory_uri() . '/assets/css/theme-style.css', array(), '0.0.1', 'all');
->>>>>>> master
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(), '4.1.3', true);
 	wp_enqueue_script('customjs', get_template_directory_uri(). '/assets/js/theme-script.js', array(), '0.0.1', true);
