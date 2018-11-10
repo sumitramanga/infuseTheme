fbfb2b7d6e7179cfef89f73fb1a6f5912bd7253c
sumitramanga <sumitramanga@gmail.com>
2018-11-10 20:01:20 +1300

    Centered onload content

--- front-page.php
+++ front-page.php
@@ -1,46 +1,48 @@
 <?php get_header(); ?>
 
-<div class="main">
-	<div class="container align-items-center text-center main-screen">
-		<?php
-			$custom_logo = get_theme_mod('custom_logo');
-			$logo_url = wp_get_attachment_image_url($custom_logo, 'medium');
-		 ?>
+<div class="main wrapper-main">
+	<div class="container text-center main-screen">
+		<div>
+			<?php
+				$custom_logo = get_theme_mod('custom_logo');
+				$logo_url = wp_get_attachment_image_url($custom_logo, 'medium');
+			 ?>
 
-		<?php if ($custom_logo): ?>
-			<div class="row h-100">
-				<div class="col- align-self-center center-content">
-					<img src="<?= $logo_url ?>" class="mainLogo" alt="Logo">
+			<?php if ($custom_logo): ?>
+				<div class="row">
+					<div class="col- center-content">
+						<img src="<?= $logo_url ?>" class="mainLogo" alt="Logo">
+					</div>
 				</div>
-			</div>
-		<?php else: ?>
-			<div class="row h-100">
-				<div class="col- align-self-center center-content">
-					<h1 class="logoText"><?= bloginfo('name'); ?></h1>
+			<?php else: ?>
+				<div class="row">
+					<div class="col- center-content">
+						<h1 class="logoText"><?= bloginfo('name'); ?></h1>
+					</div>
 				</div>
-			</div>
-		<?php endif; ?>
+			<?php endif; ?>
 
-		<?php if(get_bloginfo('description') != ''): ?>
-			<div class="row h-100">
-				<div class="col- align-self-center center-content">
-					<p class="lead">
-						<?= bloginfo('description'); ?>
-					</p>
+			<?php if(get_bloginfo('description') != ''): ?>
+				<div class="row">
+					<div class="col- center-content">
+						<p class="lead">
+							<?= bloginfo('description'); ?>
+						</p>
+					</div>
 				</div>
-			</div>
-		<?php endif; ?>
+			<?php endif; ?>
 
-		<!-- Show nav on page -->
-		<div class="align-items-center">
-			<div class="col-">
-				<?php wp_nav_menu(array(
-					'theme_location' => 'main_nav',
-					'menu_class' => 'main-menu h-100',
-					'menu-id' => 'main-menu',
-					'container_class' => 'header-menu-container'
-				));  ?>
-			</div>
+			<!-- Show nav on page -->
+			<!-- <div class="row"> -->
+				<div class="col-">
+					<?php wp_nav_menu(array(
+						'theme_location' => 'main_nav',
+						'menu_class' => 'main-menu h-100',
+						'menu-id' => 'main-menu',
+						'container_class' => 'header-menu-container'
+					));  ?>
+				</div>
+			<!-- </div> -->
 		</div>
 	</div>
 </div>
