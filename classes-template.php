9b9a7303e6295876214771acdaef146dd42b7034
isabelle may salmon <845868806@bun2-206-13.local>
2018-11-11 11:41:36 +1300

    completed classes-template.php

--- classes-template.php
+++ classes-template.php
@@ -16,7 +16,7 @@ Template Name: Classes template
 <?php if(have_posts()): ?>
 
   <?php while(have_posts()): the_post(); ?>
-      <div class="classes-template-title-wrapper"> <a href="#"><i class="classes-template-chevron fas fa-chevron-left"></i></a> <h1 class="classes-template-title"><?php the_title();  ?></h1> <span><i class="classes-template-chevron fas fa-chevron-left chevron-hidden"></i></span></div>
+      <div class="classes-template-title-wrapper"> <a href="/infuse2018"><i class="classes-template-chevron fas fa-chevron-left"></i></a> <h1 class="classes-template-title"><?php the_title();  ?></h1> <span><i class="classes-template-chevron fas fa-chevron-left chevron-hidden"></i></span></div>
       <div class="classes-template-blurb"><?php the_content();  ?></div>
   <?php endwhile; ?>
 <?php endif; ?>
@@ -52,8 +52,7 @@ Template Name: Classes template
 
        <?php if($students->have_posts()): ?>
          <?php while ($students->have_posts()): $students->the_post();?>
-           <div class="col col-custom" data-toggle="modal" data-target="#content<?php echo($post->ID); ?>">
-             <div class="hover-event-trigger">
+
 
                <?php
                 	$images =& get_children( array (
@@ -66,39 +65,44 @@ Template Name: Classes template
                 	if ( empty($images) ) {
 
                 	} else {
-                		foreach ( $images as $attachment_id => $attachment ) {
-                			echo wp_get_attachment_image( $attachment_id, 'project-thumbnail' );
-                		}
+                    ?>
+                    <div class="col col-custom" data-toggle="modal" data-target="#content<?php echo($post->ID); ?>">
+                      <div class="hover-event-trigger">
+                        <?php foreach ( $images as $attachment_id => $attachment ) {
+                          echo wp_get_attachment_image( $attachment_id, 'large', false, ['class'=>'attachment-project-thumbnail'] );
+                        } ?>
+                       <h5 class="project-title"><?php the_title(); ?></h5>
+                       <div class="white-backdrop"></div>
+                       </div>
+                   </div>
+
+
+                   <!-- Modal -->
+                   <div class="modal fade" id="content<?php echo($post->ID); ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                       <div class="modal-content">
+                         <div class="modal-header">
+                           <div class="modal-header-flex">
+                             <?php the_post_thumbnail('thumbnail', ['class'=>'project-profile-img', 'alt'=>'thumbnail image']); ?>
+                             <h5 class="modal-title" id="exampleModalLongTitle"><?php the_title(); ?></h5>
+                           </div>
+                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                             <span aria-hidden="true">&times;</span>
+                           </button>
+                         </div>
+                         <div class="modal-body">
+                           <div class="project-modal-content">
+                             <?php the_content('image'); ?>
+                           </div>
+                         </div>
+                       </div>
+                     </div>
+                   </div>
+                   <!-- End Modal -->
+                    <?php
                 	}
                 ?>
-               <h5 class="project-title"><?php the_title(); ?></h5>
-               <div class="white-backdrop"></div>
-               </div>
-           </div>
 
-
-           <!-- Modal -->
-           <div class="modal fade" id="content<?php echo($post->ID); ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-             <div class="modal-dialog modal-dialog-centered" role="document">
-               <div class="modal-content">
-                 <div class="modal-header">
-                   <div class="modal-header-flex">
-                     <?php the_post_thumbnail('thumbnail', ['class'=>'project-profile-img', 'alt'=>'thumbnail image']); ?>
-                     <h5 class="modal-title" id="exampleModalLongTitle"><?php the_title(); ?></h5>
-                   </div>
-                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                     <span aria-hidden="true">&times;</span>
-                   </button>
-                 </div>
-                 <div class="modal-body">
-                   <div class="project-modal-content">
-                     <?php the_content('image'); ?>
-                   </div>
-                 </div>
-               </div>
-             </div>
-           </div>
-           <!-- End Modal -->
 
          <?php endwhile; ?>
       <?php endif; ?>
