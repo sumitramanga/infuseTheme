81cbe2aaebf48745f335a96a6bf415b30edfefd4
isabelle may salmon <845868806@bun2-206-13.local>
2018-11-10 21:18:10 +1300

    completed styling on classes-template

---
+++ assets/js/theme-script.js
@@ -0,0 +1,12 @@
+$ = jQuery;
+
+// $( document ).ready(function() {
+//     console.log( "Jquery loaded!" );
+//
+//     $(".col-custom").hover(function(){
+//         $(".project-title").css("opacity", "1");
+//         }, function(){
+//         $(".white-backdrop").css("background-color", "rgba(255, 255, 255, 0.5)");
+//     });
+//
+// });
--- classes-template.php
+++ classes-template.php
@@ -9,19 +9,20 @@ Template Name: Classes template
 
 <?php get_header(); ?>
 
-<div class="container">
+<div class="container classes-template-container">
 
 
 
 <?php if(have_posts()): ?>
 
   <?php while(have_posts()): the_post(); ?>
-      <h1><?php the_title();  ?></h1>
-      <p><?php the_content();  ?></p>
+      <div class="classes-template-title-wrapper"> <a href="#"><i class="classes-template-chevron fas fa-chevron-left"></i></a> <h1 class="classes-template-title"><?php the_title();  ?></h1> <span><i class="classes-template-chevron fas fa-chevron-left chevron-hidden"></i></span></div>
+      <div class="classes-template-blurb"><?php the_content();  ?></div>
   <?php endwhile; ?>
 <?php endif; ?>
 
-<div class="row">
+<div class="row row-custom">
+
       <?php
         if (is_page('Web and UX')) {
           $args = array(
@@ -51,7 +52,8 @@ Template Name: Classes template
 
        <?php if($students->have_posts()): ?>
          <?php while ($students->have_posts()): $students->the_post();?>
-           <div class="col">
+           <div class="col col-custom" data-toggle="modal" data-target="#content<?php echo($post->ID); ?>">
+             <div class="hover-event-trigger">
 
                <?php
                 	$images =& get_children( array (
@@ -65,12 +67,39 @@ Template Name: Classes template
 
                 	} else {
                 		foreach ( $images as $attachment_id => $attachment ) {
-                			echo wp_get_attachment_image( $attachment_id, 'medium' );
+                			echo wp_get_attachment_image( $attachment_id, 'project-thumbnail' );
                 		}
                 	}
                 ?>
-               <h5><?php the_title(); ?></h5>
+               <h5 class="project-title"><?php the_title(); ?></h5>
+               <div class="white-backdrop"></div>
+               </div>
            </div>
+
+
+           <!-- Modal -->
+           <div class="modal fade" id="content<?php echo($post->ID); ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+             <div class="modal-dialog modal-dialog-centered" role="document">
+               <div class="modal-content">
+                 <div class="modal-header">
+                   <div class="modal-header-flex">
+                     <?php the_post_thumbnail('thumbnail', ['class'=>'project-profile-img', 'alt'=>'thumbnail image']); ?>
+                     <h5 class="modal-title" id="exampleModalLongTitle"><?php the_title(); ?></h5>
+                   </div>
+                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true">&times;</span>
+                   </button>
+                 </div>
+                 <div class="modal-body">
+                   <div class="project-modal-content">
+                     <?php the_content('image'); ?>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <!-- End Modal -->
+
          <?php endwhile; ?>
       <?php endif; ?>
     </div>
@@ -78,5 +107,7 @@ Template Name: Classes template
   </div>
 
 </div>
+
+
 
 <?php get_footer(); ?>
